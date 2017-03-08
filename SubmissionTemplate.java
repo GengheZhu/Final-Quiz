ce6fab86d2b7e434047fd6cd51f8318f63f5b3c7
Shannon Sos <ssos@pleasantonusd.net>
2017-03-08 08:58:15 -0800

    Submission Template for Java files - A17

---
+++ SubmissionTemplate.java
@@ -0,0 +1,25 @@
+
+/**
+ * Please submit your Sorts lab in the following form
+ */
+public class SubmissionTemplate
+{
+    /**
+    * Name of your class: LastName-FirstName-Period-#.java
+    * 
+    * Class includes sorts WITH STEP COUNTERS ADDED IN
+    * 
+    * Following table should be filled out AT TOP OF CLASS:
+    * 
+    * 
+    *                   100 Integers        200 Integers        400 Integers        800 Integers
+    * Bubble
+    *
+    * Selection
+    * 
+    * Insertion
+    * 
+    * 
+    * Write a 1-2 sentence write-up about your observations.
+    */
+}
