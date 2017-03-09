52a27f96c5a82225cbd87c524756328288313dcb
Vishnu Kalidas <vkpokemon@gmail.com>
2017-03-08 18:53:40 -0800

    Kalidas-Vishnu-Period-4
    This is my step counter for sorting. Have fun!

---
+++ Kalidas-Vishnu-Period-4.java
@@ -0,0 +1,14 @@
+package com.vishnu;
+
+import java.util.*;
+
+/**
+ *  Description of the Class
+ *
+ *
+ *              100 integers  200 integers  400 integers  800 integers
+ *BubbleSort    297            597          1197            2397
+ *
+ *SelectionSort 2475           2475         2475            2475
+ *
+ *InsertionSort 401            801          1601            2401
