a61d194d4cb2c1dd491f23f28b58d4ac87af0336
savvip <savita.a.pereira@gmail.com>
2017-03-08 21:24:57 -0800

    Added class Pereira-Savita-Period-4.java

--- SubmissionTemplate.java
+++ SubmissionTemplate.java
@@ -5,7 +5,7 @@
 public class SubmissionTemplate
 {
     /**
-    * Name of your class: LastName-FirstName-Period-#.java
+    * Pereira-Savita-Period-4.java
     * 
     * Class includes sorts WITH STEP COUNTERS ADDED IN
     * 
@@ -13,13 +13,18 @@ public class SubmissionTemplate
     * 
     * 
     *                   100 Integers        200 Integers        400 Integers        800 Integers
-    * Bubble
+    * Bubble           24510               98292                   386832             1577112
     *
-    * Selection
+    * Selection        15246               60496                   240996               961996  
     * 
-    * Insertion
+    * Insertion          5545                21339                    77241                317819
     * 
     * 
-    * Write a 1-2 sentence write-up about your observations.
+    * By looking at the data we see that Bubble Sort is the least efficient sorting algorithm, 
+    * taking the most steps no matter the number of integers. This is because it doesn't adapt to 
+    * to previous changes made in the passes. Insertion is the most efficient, because it is 
+    * founded on the statement that if a>b and b>c then a>c. Selection is in between because
+    * it had to constantly switch the smallest object with an object in the beginning. In general,
+    * the more integers being sorted, the more steps are needed to solve it.
     */
 }
