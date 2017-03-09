341885d2b5192a45c43c94c19b9f86f2bb4bea65
LuChristine <cl1010@pleasantonusd.net>
2017-03-09 07:30:53 -0800

    Lab A17.1
    lab and write-up

--- SubmissionTemplate.java
+++ SubmissionTemplate.java
@@ -5,7 +5,7 @@
 public class SubmissionTemplate
 {
     /**
-    * Name of your class: LastName-FirstName-Period-#.java
+    * Name of your class: Lu-Christine-Period-1.java
     * 
     * Class includes sorts WITH STEP COUNTERS ADDED IN
     * 
@@ -13,13 +13,17 @@ public class SubmissionTemplate
     * 
     * 
     *                   100 Integers        200 Integers        400 Integers        800 Integers
-    * Bubble
     *
-    * Selection
+    * Bubble            23462               101748              400948              1593492
+    *
+    * Selection         15246               60496               240996              961996
     * 
-    * Insertion
+    * Insertion         4963                19641               79523               313455
     * 
     * 
     * Write a 1-2 sentence write-up about your observations.
+    * As the number of sorted integers doubles, the number of steps it takes increases by approximately four times (Next#ofSteps = 4 * Previous#ofSteps).
+    * Bubble sort takes the most steps while insertion sort takes the least amount of steps.
+    *
     */
 }
